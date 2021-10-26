c791096b5f2f939cd6cf27694375dc83583fc350
anhefti@yahoo.de <Sargon2003>
2021-10-26 16:59:01 +0200

    added AppRunner

---
+++ src/jvmMain/kotlin/com/inari/firefly/AppRunner.kt
@@ -0,0 +1,32 @@
+package com.inari.firefly
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+abstract class AppRunner (
+    appTitle: String,
+    width: Int,
+    height: Int,
+    resizable: Boolean = true,
+    vsync: Boolean = true
+) {
+
+    init {
+        try {
+            val config = Lwjgl3ApplicationConfiguration()
+            config.setResizable(resizable)
+            config.setWindowedMode(width, height)
+            config.useVsync(vsync)
+            val desktopApp =  object : DesktopApp() {
+                override val title = appTitle
+                override fun init() = this@AppRunner.init()
+            }
+
+            Lwjgl3Application(desktopApp, config)
+        } catch (t: Throwable) {
+            t.printStackTrace()
+        }
+    }
+
+    abstract fun init()
+}
