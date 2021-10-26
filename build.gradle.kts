fb763223cf38442f96df7f6dfc1b00d5fd60b475
anhefti@yahoo.de <Sargon2003>
2021-10-26 16:11:30 +0200

    fixed publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,8 +38,6 @@ kotlin {
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-
-    
     sourceSets {
         val commonMain by getting {
             dependencies {
