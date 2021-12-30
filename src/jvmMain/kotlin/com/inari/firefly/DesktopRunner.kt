32aa464edb89fa890ad1b98a78e9071155b94ad6
AndreasHefti <anhefti@yahoo.de>
2021-12-30 12:40:44 +0100

    adapt resize

--- src/jvmMain/kotlin/com/inari/firefly/DesktopRunner.kt
+++ src/jvmMain/kotlin/com/inari/firefly/DesktopRunner.kt
@@ -5,11 +5,13 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 
 abstract class DesktopRunner (
     appTitle: String,
-    width: Int,
-    height: Int,
+    private val defaultWidth: Int,
+    private val defaultHeight: Int,
     resizable: Boolean = true,
     vsync: Boolean = true,
-    icon: String? = null
+    icon: String? = null,
+    private val fitBaseViewPortToScreen: Boolean = true,
+    private val centerCamera: Boolean = true
 ) {
 
     private lateinit var desktopApp: DesktopApp
@@ -18,13 +20,14 @@ abstract class DesktopRunner (
         try {
             val config = Lwjgl3ApplicationConfiguration()
             config.setResizable(resizable)
-            config.setWindowedMode(width, height)
+            config.setWindowedMode(defaultWidth, defaultHeight)
             config.useVsync(vsync)
             if (icon != null)
                 config.setWindowIcon(icon)
             desktopApp = object : DesktopApp() {
                 override val title = appTitle
                 override fun init() = this@DesktopRunner.init()
+                override fun resize(width: Int, height: Int) = this@DesktopRunner.resize(width, height)
             }
 
             Lwjgl3Application(desktopApp, config)
@@ -36,4 +39,10 @@ abstract class DesktopRunner (
     protected fun dispose() = desktopApp.dispose()
 
     abstract fun init()
+
+    protected fun resize(width: Int, height: Int) {
+        if (fitBaseViewPortToScreen)
+            desktopApp.fitBaseViewportToScreen(width, height, defaultWidth, defaultHeight, centerCamera)
+    }
+
 }
